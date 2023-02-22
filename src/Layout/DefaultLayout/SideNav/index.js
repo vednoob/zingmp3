64c89a85b3f07e3ee6e4c8d60c9e2a4b40844248
vednoob <leduchyy@gmail.com>
2023-02-22 13:45:38 +0700

    stuck overflow

--- src/Layout/DefaultLayout/SideNav/index.js
+++ src/Layout/DefaultLayout/SideNav/index.js
@@ -1,14 +1,1 @@
-import classNames from 'classnames/bind';
-import styles from './SideNav.module.scss';
-
-const cx = classNames.bind(styles);
-
-function SideNav() {
-  return (
-    <div className={cx('wrapper')}>
-      <h1>Side navvv</h1>
-    </div>
-  );
-}
-
-export default SideNav;
+export { default } from './SideNav';
